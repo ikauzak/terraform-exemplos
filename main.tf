7c7c9a2f7a17a6d20dce6ab65dc6016cec7f8b67
Bruno Masuda <bruno.masuda@agilitynetworks.com.br>
2019-07-02 15:32:19 -0300

    fix nohup

--- main.tf
+++ main.tf
@@ -3,10 +3,37 @@ provider "aws" {
 }
 
 resource "aws_instance" "masuda" {
-  ami           = "ami-005bdb005fb00e791"
-  instance_type = "t2.micro"
+  ami                    = "ami-005bdb005fb00e791"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  key_name = "acesso-masuda"
 
   tags {
-    Name = "terraform-masuda-exemplo"
+    Name = "terraform-exemplo"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
