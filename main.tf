3e6a41c120dce51e3e450b68ae3ebc96c16c1d6a
Bruno Masuda <bruno.masuda@agilitynetworks.com.br>
2019-07-03 21:11:46 -0300

    add variables

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "masuda" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   key_name = "acesso-masuda"
@@ -24,16 +24,30 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = "${var.ssh_port}"
+    to_port     = "${var.ssh_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+variable "ssh_port" {
+  description = "The port the server will open for SSH access"
+  default = 22
+}
+
+output "public_ip" {
+  value = "${aws_instance.masuda.public_ip}"
 }
